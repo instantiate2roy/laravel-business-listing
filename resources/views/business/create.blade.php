58bc2e4a437ba478f652e79fb8acf1bd2a302d95
Roy Gyaviira <gyaviiraroy8@gmail.com>
2022-09-23 00:15:31 +0800

    added incompleted create new business feature

--- resources/views/business/create.blade.php
+++ resources/views/business/create.blade.php
@@ -1,55 +1,53 @@
 @Auth
     @extends('layouts.app')
     @section('content')
-        <link href="{{ asset('css/groupsScreen.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/businessesScreen.css') }}" rel="stylesheet">
         <div class="container">
-            <div class="row">
-                <div class="col-md-3">@include('inc.sidebar')</div>
-                <div class="col-md-9">
-                    <div class='card'>
-                        <div class='card-header'>
-                            <div class="groupsBackButton">
-                                <a href="/groups?{{ $paginationPageName }}={{ $lastPage }}">
-                                    <button class="btn btn-primary">
-                                        <span class="fa fa-arrow-left" aria-hidden="true">
-                                        </span> Go Back
-                                    </button>
-                                </a>
-                            </div>
-                            <div class="groupsCardTitle">Create New Group</div>
-                        </div>
-                        <div class='card-body'>
-                            {!! Form::open(['action' => 'App\Http\Controllers\GroupsController@store', 'method' => 'POST']) !!}
-
-
-                            <div class="form-group">
-                                {!! Form::label('group_code', 'Group Code') !!}
-                                {!! Form::text('group_code', '', ['class' => 'form-control','Placeholder'=>'Group Code']) !!}
-                            </div>
-                            <br>
-
-                            <div class="form-group">
-                                {!! Form::label('group_name', 'Group Name') !!}
-                                {!! Form::text('group_name', '', ['class' => 'form-control', 'placeholder' => 'Group Name']) !!}
-                            </div>
-                            <br>
-
-                            <div class="form-group">
-                                {!! Form::label('group_status', 'Group Status') !!}
-
-                                {!! Form::select('group_status', $groupStatusLookups, '', [
-                                    'class' => 'form-control',
-                                ]) !!}
-                            </div>
-                            <br>
-                            {!! Form::submit('Submit', ['class' => 'btn btn-primary']) !!}
-                            {!! Form::close() !!}
-                        </div>
-                        <div class='card-footer'></div>
+            <div class='card'>
+                <div class='card-header'>
+                    <div class="businessesBackButton">
+                        <a href="/businesses?{{ $paginationPageName }}={{ $lastPage }}">
+                            <button class="btn btn-primary">
+                                <span class="fa fa-arrow-left" aria-hidden="true">
+                                </span> Go Back
+                            </button>
+                        </a>
                     </div>
-
+                    <div class="businessesCardTitle">Create New Business</div>
                 </div>
+                <div class='card-body'>
+                    {!! Form::open([
+                        'action' => 'App\Http\Controllers\BusinessesController@store',
+                        'method' => 'POST',
+                        'enctype' => 'multipart/form-data',
+                    ]) !!}
+
+
+                    <div class="form-group">
+                        {!! Form::label('biz_code', 'Business Code') !!}
+                        {!! Form::text('biz_code', '', ['class' => 'form-control', 'Placeholder' => 'Business Code']) !!}
+                    </div>
+                    <br>
+
+                    <div class="form-group">
+                        {!! Form::label('biz_name', 'Business Name') !!}
+                        {!! Form::text('biz_name', '', ['class' => 'form-control', 'placeholder' => 'Business Name']) !!}
+                    </div>
+                    <br>
+
+                    <div class="form-group">
+                        {{ Form::label('biz_image_path', 'Business Display Image') }}
+                        {{ Form::file('biz_image_path') }}
+                    </div>
+                    <br>
+                    {!! Form::submit('Submit', ['class' => 'btn btn-primary']) !!}
+                    {!! Form::close() !!}
+                </div>
+                <div class='card-footer'></div>
             </div>
+
+
+
         </div>
     @endsection
 @endAuth
