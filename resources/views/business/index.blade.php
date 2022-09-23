3267672535b605d62dcf8ee903243291a3a2ce80
Roy Gyaviira <gyaviiraroy8@gmail.com>
2022-09-23 22:56:16 +0800

    view creation first instance

--- resources/views/business/index.blade.php
+++ resources/views/business/index.blade.php
@@ -23,34 +23,10 @@
                         {{ $businesses->links() }}
                         <br>
                         <hr>
-                        <br>
-
-
-
-
 
                         @foreach ($businesses as $business)
-                            <div
-                                class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-                                <div class="col-auto d-none d-lg-block">
-                                    <img class="bd-placeholder-img" width="200" height="250"
-                                        src="/storage/{{ $business->biz_image_path }}" role="img" aria-label="Placeholder: Thumbnail"
-                                        preserveAspectRatio="xMidYMid slice" focusable="false">
-
-                                </div>
-                                <div class="col p-4 d-flex flex-column position-static">
-                                    <strong class="d-inline-block mb-2 text-success">{{ $business->name }}</strong>
-                                    
-
-
-
-                                </div>
-
-                            </div>
+                            
                         @endforeach
-
-
-                        <br>
                         <hr>
                         <br>
                         {{ $businesses->links() }}
